8a00c50a1d435c1c27619a43377e14c4b51422ec
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-03-22 13:11:12 +0900

    3/22 수업(2.2절 완료)

--- src/main/java/com/example/demo/controller/TodoController.java
+++ src/main/java/com/example/demo/controller/TodoController.java
@@ -1,21 +1,39 @@
 package com.example.demo.controller;
 
-import com.example.demo.dto.TodoDTO;
+import com.example.demo.dto.ResponseDTO;
+import com.example.demo.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("todo")
 public class TodoController {
+    @Autowired
+    private TodoService service;
+
+    @GetMapping("/test")
+    public ResponseEntity<?> testTodo() {
+        String str = service.testService();
+
+        List<String> list = new ArrayList<>();
+        list.add(str);
+
+        ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+        return ResponseEntity.ok().body(response);
+    }
+     /*
     @GetMapping("/testTodo")
     public ResponseEntity<?> testTodo() {
 
         TodoDTO response = TodoDTO.builder().id("123").title("김다희").done(true).build();
         return ResponseEntity.ok().body(response);
 
-        /*
         다른 방법
         TodoDTO todoDTO = new TodoDTO();
 
@@ -27,5 +45,4 @@ public class TodoController {
 
         */
 
-    }
 }
--- src/main/java/com/example/demo/model/TodoEntity.java
+++ src/main/java/com/example/demo/model/TodoEntity.java
@@ -4,12 +4,24 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
+@Entity
+//Todo테이블과 mapping 됨
+@Table(name = "Todo") //명시하지 않으면 클래스 명이 테이블 이름이 됨
 public class TodoEntity {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name="system-uuid", strategy="uuid")
     private String id; //이 오브젝트의 아이디
     private String userId; //이 오브젝트를 생성한 사용자의 아이디
     private String title; // Todo 타이틀
