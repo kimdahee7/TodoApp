c38512fe58745eb36ca5a11e116421b3e28a39a3
kimdahee7 <kikidahee7@duksung.ac.kr>
2023-03-27 16:19:04 +0900

    3/27 수업(2.3절 retrieve 완료)

--- src/main/java/com/example/demo/service/TodoService.java
+++ src/main/java/com/example/demo/service/TodoService.java
@@ -2,9 +2,13 @@ package com.example.demo.service;
 
 import com.example.demo.model.TodoEntity;
 import com.example.demo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
     @Autowired
@@ -17,5 +21,24 @@ public class TodoService {
         //TodoEntity 검색
         TodoEntity savedEntity = repository.findById(entity.getId()).get();
         return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        if(entity == null) {
+            log.warn("Entity cannot be null");
+            throw new RuntimeException("Entity cannot be null");
+        }
+        if(entity.getUserId() == null) {
+            log.warn("Unknown user.");
+            throw new RuntimeException("Unknown user.");
+        }
+        repository.save(entity);
+        log.info("Entity id: {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+    }
+
+    public List<TodoEntity> retrieve(final String userId) {
+        return repository.findByUserId(userId);
     }
 }
